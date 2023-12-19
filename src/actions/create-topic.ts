2425eb8fa273175eba6604944632c81d5b50ad07
nicolofabris <nicolo.fabriss@gmail.com>
2023-12-19 12:45:54 +0100

    error handling

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,16 +1,70 @@
 'use server'
 
+import type {Topic} from '@prisma/client'
+import { redirect } from 'next/navigation'
 import {z} from 'zod'
+import {auth} from '@/auth'
+import { db } from '@/db'
+import paths from '@/paths'
+import { revalidatePath } from 'next/cache'
 
 const createTopicSchema = z.object({
     name: z.string().min(3).regex(/[a-z-]/, {message: 'Must be lowercase letters'}),
     description: z.string().min(10)
 })
 
-export async function createTopic(formState: number, formData: FormData) {
+interface CreateTopicFormState {
+    errors: {
+        name?: string[],
+        description?: string[],
+        _form?: string[],
+    }
+}
+ 
+export async function createTopic(formState: CreateTopicFormState, formData: FormData): Promise<CreateTopicFormState> {
     const result = createTopicSchema.safeParse({
         name: formData.get('name'),
         description: formData.get('description'),
     })
-    return 10
+    if (!result.success) {
+        return {
+            errors: result.error.flatten().fieldErrors
+        }
+    }
+
+    const session = await auth()
+    if (!session || !session.user) {
+        return {
+            errors: {
+                _form: ['You must be signed in']
+            }
+        }
+    }
+    let topic: Topic
+    try {
+        topic = await db.topic.create({
+            data: {
+                slug: result.data.name,
+                description: result.data.description
+            }
+        })
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            return {
+                errors: {
+                    _form: [error.message]
+                }
+            }
+        } else {
+            return {
+                errors: {
+                    _form: ['Something went wrong']
+                }
+            }
+        }
+    }
+
+    revalidatePath('/')
+
+    redirect(paths.topicShow(topic.slug))
 }
--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { useFormState } from "react-dom";
 import {
@@ -12,7 +12,7 @@ import {
 import * as actions from "@/actions";
 
 export default function TopicCreateForm() {
-    const [formState, action] = useFormState(actions.createTopic, 5)
+  const [formState, action] = useFormState(actions.createTopic, { errors: {} });
 
   return (
     <Popover placement="left">
@@ -23,12 +23,28 @@ export default function TopicCreateForm() {
         <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg">Create a topic</h3>
-            <Input name='name' label="Name" labelPlacement="outside" placeholder="Name" />
-            <Textarea name="description"
+            <Input
+              name="name"
+              label="Name"
+              labelPlacement="outside"
+              placeholder="Name"
+              isInvalid={!!formState.errors.name}
+              errorMessage={formState.errors.name?.join(', ')}
+            />
+            
+            <Textarea
+              name="description"
               label="Description"
               labelPlacement="outside"
               placeholder="Describe your topic"
+              isInvalid={!!formState.errors.description}
+              errorMessage={formState.errors.description?.join(', ')}
             />
+
+
+            {formState.errors._form ?.join(', ')}
+
+
             <Button type="submit">Submit</Button>
           </div>
         </form>
